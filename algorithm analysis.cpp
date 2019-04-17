ade7e83a17aeda54f0846db794069283f8b567ad
Jonathon <34872749+GoodStuff11@users.noreply.github.com>
2019-04-17 00:31:40 -0400

    Update algorithm analysis.cpp

--- algorithm analysis.cpp
+++ algorithm analysis.cpp
@@ -1,27 +1,74 @@
-#define SIZE 10000
 #include <iostream>
 #include <stdlib.h>
-#include "sorting algorithm.cpp"
-
+#include "sorting algorithm.h"
+#include <fstream>
 using namespace std;
 
-void print_array(int* array,int size){
+void print_array(int* array, int size) {
 	cout << "{";
-	for (int i=0;i<size;i++){
+	for (int i = 0; i < size; i++) {
 		cout << array[i];
-		if (i<size-1)
+		if (i < size - 1)
 			cout << ',';
 	}
 	cout << "}" << endl;
 }
-int main() {
-	int x[SIZE] = new int[SIZE];
-	for (int i=0;i<SIZE;i++){
-		x[i] = rand();
+double average(int* array, int size) {
+	double sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += (double) array[i];
 	}
+	return sum / (double) size;
+}
+int main() {
+	algorithm *sort = new algorithm();
 
-	//mergeSort(x,0,size-1);
-	IntroSort(x,SIZE);
-	print_array(x,SIZE);
+	ofstream file("sortingSorted.csv", ios::out);
+
+	int size = 1;
+	file
+			<< "Array size,HeapSort comparisons,MergeSort comparisons,InsertionSort comparisons"
+			<< endl;
+	for (int k = 0; size < 50000; k++) {
+		size *= 2;
+		file << size << ',';
+		cout << size << endl;
+
+		int merge[100];
+		int heap[100];
+		int insertion[100];
+		for (int reps = 0; reps < 100; reps++) {
+			int *x = new int[size];
+			int *y = new int[size];
+			int *z = new int[size];
+			for (int i = 0; i < size; i++) {
+				//x[i] = rand();
+				//x[i] = size - i;
+				x[i] = i;
+				y[i] = x[i];
+				z[i] = x[i];
+			}
+
+			sort->comparisons = 0;
+			sort->HeapSort(x, size);
+			heap[reps] = sort->comparisons;
+
+			sort->comparisons = 0;
+			sort->mergeSort(y, 0, size - 1);
+			merge[reps] = sort->comparisons;
+
+			sort->comparisons = 0;
+			sort->InsertionSort(z, size);
+			insertion[reps] = sort->comparisons;
+
+			delete x;
+			delete y;
+			delete z;
+		}
+		file << average(heap, 100) << ',';
+		file << average(merge, 100) << ',';
+		file << average(insertion, 100) << endl;
+	}
+	file.close();
 	return 0;
 }
